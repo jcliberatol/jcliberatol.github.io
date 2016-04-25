15d4d40ca94c55389c3eb246ed4dde8ea6d64026
Juan Liberato <juan.liberato@lindencolombia.com>
2016-04-25 08:15:12 -0500

    corrected events of tutorial

---
+++ js/expandable.js
@@ -0,0 +1,15 @@
+$('.expandableContainer').on('mouseleave',function () {
+  $(this).children().each(function (x) {
+    if ($(this).hasClass("expandableSection")) {
+      $(this).css("display","none");
+    }
+  })
+})
+
+$('.expandableContainer').on('mouseenter',function (e) {
+  $(this).children().each(function (x) {
+    if ($(this).hasClass("expandableSection")) {
+      $(this).css("display","block");
+    }
+  })
+})
